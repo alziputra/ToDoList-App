c392514b8a653393108cb9b4685932c4abb42c56
alziputra <alziputra12@gmail.com>
2023-10-29 23:04:07 +0700

    update col list todo

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -21,7 +21,7 @@ function ListTodos() {
   };
 
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <ul className="grid grid-cols-1 gap-4">
       {todos.map((todo) => (
         <li key={todo.id} className="flex items-center bg-gray-200 p-2 rounded-md">
           <span onClick={() => handleToggle(todo.id)} className={`cursor-pointer flex-1 ${todo.completed ? "line-through text-gray-400" : ""}`}>
